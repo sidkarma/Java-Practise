168581db710e157c8e6f340c942fffa84fcc1203
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-02 20:44:11 +0530

    Update recProb6.java

--- recProb6.java
+++ recProb6.java
@@ -1,8 +1,26 @@
 import java.util.*;
 // Remove all duplicates in a string
 public class recProb6 {
-    public static void removeDups(String str, int idx)
-    public static void main(String args[]) {
+    public static boolean[] map = new boolean[26];
 
+    public static void removeDups(String str, int idx, String newStr) {
+        if(idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar-'a']) {
+            removeDups(str, idx+1, newStr);
+        } else {
+            newStr += currChar;
+            map[currChar-'a'] = true;
+            removeDups(str, idx+1, newStr);
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = "abbcccda";
+        sc.close();
+        removeDups(str, 0, "");
     }
 }
