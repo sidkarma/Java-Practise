66c77d69d19e320c87a6e0895a4dc2b36987774b
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-03 20:50:41 +0530

    Create recProb9.java

---
+++ recProb9.java
@@ -0,0 +1,25 @@
+import java.util.*;
+// Print keypad combinations
+public class recProb9 {
+    public static String[] keypad = {".","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+
+    public static void printCombs(String str,int idx, String comb) {
+        if(idx == str.length()) {
+            System.out.println(comb);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        String mapping = keypad[currChar-'0'];
+
+        for(int i=0;i<mapping.length();i++) {
+            printCombs(str, idx+1, comb+mapping.charAt(i));
+        }
+
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = "23";
+        printCombs(str, 0, "");
+    }
+}
