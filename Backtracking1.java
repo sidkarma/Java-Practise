93093535e6b9b353f7ad8cb4f6724c3ade107d3d
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-07 21:45:41 +0530

    Create Backtracking1.java

---
+++ Backtracking1.java
@@ -0,0 +1,11 @@
+// Backtracking
+
+import java.util.*;
+// 
+public class recProb15 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
