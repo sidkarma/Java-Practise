cc58bae348499c6bb09e3d2fd069491ea5722f1c
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-08 22:37:30 +0530

    Update Backtracking1.java

--- Backtracking1.java
+++ Backtracking1.java
@@ -2,10 +2,23 @@
 
 import java.util.*;
 // 
-public class recProb15 {
+public class Backtracking1 {
+    public static void printPermu(String str, String perm, int idx) {
+        if(str.length()==0) {
+            System.out.println(perm);
+            return;
+        }
+        for(int i=0;i<str.length();i++) {
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i)+str.substring(i+1);
+            printPermu(newStr, perm + currChar, idx+1);
+        }
+    }
+    
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
+        String str = "ABC";
+        printPermu(str, "", 0);
         sc.close();
     }
 }
