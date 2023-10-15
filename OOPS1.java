26cac22d79f4b3163149a9e1b1e20383a2ed6bcd
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-15 21:50:29 +0530

    Create OOPS1.java

---
+++ OOPS1.java
@@ -0,0 +1,16 @@
+class Pen {
+    String color;
+    String type; // ballpoint/gel
+
+    public void() {
+        System.out.println("Writing simething");
+    }
+}
+
+public class OOPS1 {
+    public static void main(String args[]) {
+        Pen ob = new Pen();
+        
+    }
+    
+}
