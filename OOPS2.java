67f127ac018cc44f9059e574ed4126daa443559d
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-16 16:28:43 +0530

    Update OOPS2.java

--- OOPS2.java
+++ OOPS2.java
@@ -9,14 +9,13 @@ class Triangle extends Shape {
         System.out.println(1/2*l*h);
     }
 }
-class EquilateralTriangle extends Triangle {
-    public void area(int l, int h) {
-        System.out.println(1/2*l*h);
+class Circle extends Shape {
+    public void area(int r) {
+        System.out.println((3.14)*r*r);
     }
 }
 
 public class OOPS2 {
     public static void main(String args[]) {
-        Triangle t1 = new Triangle();
     }
 }
