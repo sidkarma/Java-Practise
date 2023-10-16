98069e6c8de7e26977e791bd6a8ed703f8b20e8f
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-16 16:24:09 +0530

    Update OOPS2.java

--- OOPS2.java
+++ OOPS2.java
@@ -1,7 +1,22 @@
 // Inheritance
+class Shape {
+    public void area() {
+        System.out.println("displays area");
+    }
+}
+class Triangle extends Shape {
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
+class EquilateralTriangle extends Triangle {
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
 
 public class OOPS2 {
     public static void main(String args[]) {
-        
+        Triangle t1 = new Triangle();
     }
 }
