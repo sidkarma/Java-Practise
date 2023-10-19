e67d78df7ad7883ed4371d420ba9a4c6ba6c8064
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-19 22:38:25 +0530

    Update OOPS2.java

--- OOPS2.java
+++ OOPS2.java
@@ -1,5 +1,6 @@
 // OOPS
 // Inheritance
+import bank;
 class Shape {
     public void area() {
         System.out.println("displays area");
@@ -18,5 +19,7 @@ class Circle extends Shape {
 
 public class OOPS2 {
     public static void main(String args[]) {
+        bank.Account account1 = new bank.Account();
+        account1.name = "customer1";
     }
 }
