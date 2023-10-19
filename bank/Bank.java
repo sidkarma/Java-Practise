a174533044ecadf7330d8ace483c0f349030f968
Siddhant Vishwakarma <siddhantvishwakarma5@gmail.com>
2023-10-19 22:48:00 +0530

    Update Bank.java

--- bank/Bank.java
+++ bank/Bank.java
@@ -4,10 +4,14 @@ package bank;
 
 class Account {
     public String name;
+    protected String email;
 }
 
 public class Bank {
     public static void main(String args[]) {
         Account account1 = new Account();
+        account1.name = "SidKarma";
+        account1.email = "sidkarma@gmail.com";
+
     }
 }
