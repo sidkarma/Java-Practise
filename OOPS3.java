a400922e52e24759567aa0687c4d1680710e051a
sidkarma <siddhantvishwakarma5@gmail.com>
2023-10-30 19:10:13 +0530

    Update OOPS3.java

---
+++ OOPS3.java
@@ -0,0 +1,15 @@
+class Animals {
+    public void walk() {
+
+    }
+}
+
+class Horse extends Animals {
+    public void walk() {
+        System.out.println("Walks on 4 legs"):
+    }
+}
+
+public class OOPS3 {
+    public static void main(String args)
+}
