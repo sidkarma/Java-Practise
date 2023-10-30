4a90c7f1afdc52c2e57955ceac58029ab1e00e42
sidkarma <siddhantvishwakarma5@gmail.com>
2023-10-30 19:28:09 +0530

    Update OOPS2.java

--- OOPS2.java
+++ OOPS2.java
@@ -1,19 +1,23 @@
+
 // OOPS
 // Inheritance
 import bank;
+
 class Shape {
     public void area() {
         System.out.println("displays area");
     }
 }
+
 class Triangle extends Shape {
     public void area(int l, int h) {
-        System.out.println(1/2*l*h);
+        System.out.println(1 / 2 * l * h);
     }
 }
+
 class Circle extends Shape {
     public void area(int r) {
-        System.out.println((3.14)*r*r);
+        System.out.println((3.14) * r * r);
     }
 }
 
