7bee4652fceb0fe3066c895d2bd43f3ea52c1801
sidkarma <siddhantvishwakarma5@gmail.com>
2024-01-30 19:58:53 +0530

    Create LinkedList1.java

---
+++ LinkedList1.java
@@ -0,0 +1,18 @@
+public class LinkedList1 {
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add - first, last
+    
+
+    public static void main(String[] args) {
+        LinkedList1 list = new LinkedList1();
+    }
+}
