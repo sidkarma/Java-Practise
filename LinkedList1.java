27e7cdfc536b186b4d21373ce6c35f97dfc6a7bb
sidkarma <siddhantvishwakarma5@gmail.com>
2024-01-31 13:54:23 +0530

    update

--- LinkedList1.java
+++ LinkedList1.java
@@ -1,4 +1,6 @@
 public class LinkedList1 {
+    Node head;
+
     class Node {
         String data;
         Node next;
@@ -10,9 +12,63 @@ public class LinkedList1 {
     }
 
     // add - first, last
-    
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+    }
+
+    // print
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
+    }
+
+    // delete first
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("The list is empty");
+            return;
+        }
+    }
 
     public static void main(String[] args) {
         LinkedList1 list = new LinkedList1();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+
+        list.addLast("list");
+        list.printList();
+
+        list.addFirst("this");
+        list.printList();
     }
 }
