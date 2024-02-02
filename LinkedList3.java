5c6119cbfcebc454800593c3245703ab4bcf7b57
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-02 20:01:06 +0530

    update

--- LinkedList3.java
+++ LinkedList3.java
@@ -1,27 +1,73 @@
-import java.util.*;
+import java.util.LinkedList;
 
-public void reverseIterate() {
+public class LinkedList3 {
+    static class Node {
+        int data;
+        Node next;
 
-
-    Node prevNode = head;
-    Node currNode = head.next;
-    while(currNode != null) {
-        Node nextNode = currNode.next;
-        currNode.next = prevNode;
-
-        //update
-        prevNode = currNode;
-        currNode = nextNode;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-}
 
-class LinkedList3 {
+    Node head;
+
+    public void reverseIterate() {
+        if (head == null || head.next == null) {
+            return; // No need to reverse if the list is empty or has only one element
+        }
+
+        Node prevNode = head;
+        Node currNode = head.next;
+        while (currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null; // Set the next of the original head to null
+        head = prevNode; // Update the head to point to the new first node
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
+    }
+
     public static void main(String[] args) {
         LinkedList3 list = new LinkedList3();
         list.addLast(1);
         list.addLast(2);
         list.addLast(3);
         list.addLast(4);
+
+        System.out.println("Original List:");
+        list.printList();
+
+        System.out.println("Reversed List:");
+        list.reverseIterate();
         list.printList();
     }
 }
