4698c3b367b7438351ed573d59499b1631a1a7c9
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-07 19:31:00 +0530

    update

--- sortSelection.java
+++ sortSelection.java
@@ -1,19 +1,19 @@
 public class sortSelection {
-    
+
     public static void printArr(int arr[]) {
-        for(int i=0;i<arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
 
     public static void main(String args[]) {
-        int arr[] = {7,8,3,1,2};
+        int arr[] = { 7, 8, 3, 1, 2 };
 
-        for(int i=0;i<arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             int min = i;
-            for(int j=i+1;j<arr.length;j++) {
-                if(arr[min]>arr[j]) {
-                    min=j;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
                 }
             }
             int temp = arr[min];
--- stack1.java
+++ stack1.java
@@ -1,7 +1,10 @@
+// stack as LinkedList
+
 public class stack1 {
     static class Node {
         int data;
         Node next;
+
         public Node(int data) {
             this.data = data;
             next = null;
@@ -9,15 +12,50 @@ public class stack1 {
     }
 
     static class Stack {
-        public Node head;
-        public static void push(int data) {
-            if(head == null) {
-                
-            }
+        public static Node head;
+
+        public static boolean isEmpty() {
+            return head == null;
         }
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+
     }
 
     public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
 
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
