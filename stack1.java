347169df81042e0f367f5b6c69469dab9a2861c8
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-07 18:49:00 +0530

    update

---
+++ stack1.java
@@ -0,0 +1,23 @@
+public class stack1 {
+    static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    static class Stack {
+        public Node head;
+        public static void push(int data) {
+            if(head == null) {
+                
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
