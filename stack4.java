86623fbab00846fbe389d3c085d9756a0e9377e6
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-09 15:31:24 +0530

    update

--- stack4.java
+++ stack4.java
@@ -1,12 +1,24 @@
 import java.util.Stack;
 
 public class stack4 {
+    public static void pushAtBottom(int data, Stack<Integer> s) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+
+    }
+
     public static void main(String args[]) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-        s.push(4);
+
+        pushAtBottom(4, s);
 
         while (!s.isEmpty()) {
             System.out.println(s.peek());
