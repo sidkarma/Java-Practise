bb2aa5810bc47286dcbb629be0518c3989bc8f8f
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-19 19:29:48 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,10 +2,11 @@
 
 //  - Case 1: Diameter (Longest path) passes through Root
 //  - Case 2: Diameter (Longest path) does not pass through Root
-//  Approach 1: O(n2)
+//  Approach 1: O(n^2)
 //      -> Diam 1 - Largest dia in Left Subtree
 //      -> Diam 2 - Largest dia in Right Subtree
 //      -> Diam 3 - leftHeight + rightHeight + 1
+//      -> Actual Diameter of the tree = Max(Diam 1,Diam 2,Diam 3)
 
 public class BinaryTree {
 
