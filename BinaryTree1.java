d1a80b19969a5b0d07a1d6e009b8f5302e388954
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-19 16:12:47 +0530

    update

--- BinaryTree1.java
+++ BinaryTree1.java
@@ -37,12 +37,23 @@ public class BinaryTree1 {
         preOrder(root.right);
     }
 
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         // System.out.println(root.data);
 
-        preOrder(root);
+        // preOrder(root);
+        inOrder(root);
     }
 }
