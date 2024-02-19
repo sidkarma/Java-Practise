66f22fa09aaad0e3c55442d0af930ae391d03685
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-19 19:22:27 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,7 +1,7 @@
 // Diameter of a Tree - Number of Nodes in the Longest path between any 2 nodes.
 
-//  - Case 1: Diameter through Root
-//  - Case 2: Diameter does not pass through Root
+//  - Case 1: Diameter (Longest path) passes through Root
+//  - Case 2: Diameter (Longest path) does not pass through Root
 
 public class BinaryTree {
 
