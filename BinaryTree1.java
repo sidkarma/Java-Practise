1d1c99349972ed3ce4978987d5af5bcdd984e33f
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-19 16:49:21 +0530

    update

--- BinaryTree1.java
+++ BinaryTree1.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BinaryTree1 {
     static class Node {
         int data;
@@ -58,6 +60,35 @@ public class BinaryTree1 {
         System.out.print(root.data + " ");
     }
 
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
@@ -66,6 +97,7 @@ public class BinaryTree1 {
 
         // preOrder(root);
         // inOrder(root);
-        postOrder(root);
+        // postOrder(root);
+        levelOrder(root);
     }
 }
