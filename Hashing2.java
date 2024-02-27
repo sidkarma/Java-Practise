f8090d0ab7c05dd87b60632846246b83e31e030d
sidkarma <siddhantvishwakarma5@gmail.com>
2024-02-27 20:07:16 +0530

    update

--- Hashing2.java
+++ Hashing2.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Hashing2 {
     public static void main(String[] args) {
@@ -15,15 +17,35 @@ public class Hashing2 {
         hm.put("China", 180);
         System.out.println(hm);
 
-        // Search - Look up
-        if (hm.containsKey("Indonesia")) {
-            System.out.println("key is present in the map");
-        } else {
-            System.out.println("key is not present");
-        }
+        // // Search - Look up
+        // if (hm.containsKey("Indonesia")) {
+        // System.out.println("key is present in the map");
+        // } else {
+        // System.out.println("key is not present");
+        // }
 
-        // get
-        System.out.println(hm.get("China"));
-        System.out.println(hm.get("Indonesia"));
+        // // get
+        // System.out.println(hm.get("China"));
+        // System.out.println(hm.get("Indonesia"));
+
+        // int arr[] = { 12, 15, 18 };
+        // for (int i : arr) {
+        // System.out.print(i + " ");
+        // }
+
+        // // Iteration in HashMap
+        // for (Map.Entry<String, Integer> e : hm.entrySet()) {
+        // System.out.print(e.getKey() + " : ");
+        // System.out.println(e.getValue());
+        // }
+
+        // Set<String> keys = hm.keySet();
+        // for (String key : keys) {
+        // System.out.println(key + " " + hm.get(key));
+        // }
+
+        // Remove pair
+        hm.remove("China");
+        System.out.println(hm);
     }
 }
