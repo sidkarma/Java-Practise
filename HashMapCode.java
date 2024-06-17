9177e5edcd2e5b9a0f863ee67d8f003a7525ccda
sidkarma <siddhantvishwakarma5@gmail.com>
2024-06-17 15:03:01 +0530

    update

---
+++ HashMapCode.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class HashMapCode {
+    static class HashMap<K, V> {
+        private class Node {
+            K key;
+            V value;
+
+            public Node(K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private int n; // n
+        private int N;
+        private LinkedList<Node> buckets[]; // N = buckets.length
+
+        @SuppressWarnings("unchecked")
+        public HashMap() {
+            this.N = 4;
+            this.buckets = new LinkedList[4];
+            for (int i = 0; i < 4; i++) {
+                this.buckets[i] = new LinkedList<>();
+            }
+        }
+    }
+}
