6a885d198acc72d94ca14c56264b290dedb4c647
sidkarma <siddhantvishwakarma5@gmail.com>
2024-06-18 16:17:44 +0530

    Update HashMapCode.java

--- HashMapCode.java
+++ HashMapCode.java
@@ -12,8 +12,8 @@ public class HashMapCode {
             }
         }
 
-        private int n; // n
-        private int N;
+        private int n; // n -> nodes
+        private int N; // N -> buckets
         private LinkedList<Node> buckets[]; // N = buckets.length
 
         @SuppressWarnings("unchecked")
@@ -23,6 +23,36 @@ public class HashMapCode {
             for (int i = 0; i < 4; i++) {
                 this.buckets[i] = new LinkedList<>();
             }
+        }
+
+        private int hashFunction(K key) {
+
+        }
+
+        public void put(K key, V value) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi); // data index
+
+            if (di == -1) { // key doesn't exist
+                buckets[bi].add(new Node(key, value));
+                n++;
+            } else { // key exists
+                Node data = buckets[bi].get(di);
+                data.value = value;
+            }
+
+            double lambda = (double) n / N;
+            if (lambda > 2.0) {
+                // rehashing
+            }
+        }
+
+        public boolean containsKey(K key) {
+            return false;
+        }
+
+        public V remove(K key) {
+            return null;
         }
     }
 }
