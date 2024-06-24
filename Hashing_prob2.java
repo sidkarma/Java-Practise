ba3ab85e55971f2b1113da4eb53d681474fa0157
sidkarma <siddhantvishwakarma5@gmail.com>
2024-06-24 15:29:07 +0530

    update

--- Hashing_prob2.java
+++ Hashing_prob2.java
@@ -1,5 +1,27 @@
 // Union of 2 arrays
 
+import java.util.*;
+
 public class Hashing_prob2 {
+    public static int union(int arr1[], int arr2[]) { // O(n)
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+
+        for (int j = 0; j < arr2.length; j++) {
+            set.add(arr2[j]);
+        }
+
+        return set.size();
+    }
+
+    public static void main(String args[]) {
+        int arr1[] = { 7, 3, 9 };
+        int arr2[] = { 6, 3, 9, 2, 9, 4 };
+
+        System.out.println(union(arr1, arr2));
+    }
 
 }
