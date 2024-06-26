9b6747428146e12826ce07362415a6ccff6dee9b
sidkarma <siddhantvishwakarma5@gmail.com>
2024-06-26 16:06:21 +0530

    update

--- Hashing_prob5.java
+++ Hashing_prob5.java
@@ -4,6 +4,27 @@ import java.util.*;
 
 public class Hashing_prob5 {
     public static void main(String[] args) {
+        int arr[] = { 10, 2, -2, -20, 10 }; // ans = 3
+        int k = -10;
+        HashMap<Integer, Integer> map = new HashMap<>(); // <Sum, Freq>
 
+        map.put(0, 1); // empty sub-array
+        int ans = 0;
+        int sum = 0;
+
+        for (int j = 0; j < arr.length; j++) {
+            sum += arr[j];
+
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+
+            if (map.containsKey(sum)) {
+                map.put(sum, map.get(sum) + 1);
+            } else {
+                map.put(sum, 1);
+            }
+        }
+        System.out.println(ans);
     }
 }
